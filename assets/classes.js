48fff9686ab9e4315a47192fd3488b873958f003
Brennan <brennanheley@gmail.com>
2022-04-03 20:47:15 -0500

    fixed history buttons

--- assets/classes.js
+++ assets/classes.js
@@ -1,15 +1,20 @@
 class WeatherUI {
-  constructor() {
-    // show history buttons on load
+  constructor(client) {
+    this.client = client;
     this.showNewHistory();
+    this.refreshHistoryClickListener(client);
 
-    // add click button event handler
-    $("button.clear-btn").click(() => {
+    $("button.clear-btn").on("click", () => {
       window.localStorage.removeItem(LS_KEY);
       this.showNewHistory();
       $('input[name="search-query"]').val("");
       $('input[name="search-query"]').focus();
     });
+  }
+
+  getHistory() {
+    const stringFromStorage = window.localStorage.getItem(LS_KEY) || "[]";
+    return JSON.parse(stringFromStorage);
   }
 
   getUviSeverity = (uvi) => {
@@ -18,27 +23,63 @@ class WeatherUI {
     return "mild";
   };
 
-  getHistory() {
-    const stringFromStorage = window.localStorage.getItem(LS_KEY) || "[]";
-    return JSON.parse(stringFromStorage);
+  refreshHistoryClickListener(client) {
+    $(".history .history-btn").off();
+    $(".history").on("click", ".history-btn", function () {
+      const savedQuery = $(this).text();
+      client.search(savedQuery);
+    });
   }
 
   showNewHistory(history = this.getHistory()) {
-    const newInnerHTML = history
+    const buttonsHTML = history
       .map(
         (query) => `<button class="history-btn" type="button">${query}</button>`
       )
       .join("");
-    $(".search > .history").html(newInnerHTML);
+    $(".search > .history").html(buttonsHTML);
+    this.refreshHistoryClickListener(this.client);
   }
 
   updateHistory(newItem) {
-    // save last 10 unique user searches
     let history = this.getHistory();
     if (!history.includes(newItem)) history.unshift(newItem);
     if (history.length > 10) history = history.slice(0, 10);
     localStorage.setItem(LS_KEY, JSON.stringify(history));
     this.showNewHistory(history);
+  }
+
+  updateFiveDayForecast(fiveDay) {
+    const forecastHTML = fiveDay
+      .map(
+        ({ icon, date, temp, wind, humidity }, idx) =>
+          `<div class="card">
+						<h3>${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}</h3>
+						<img src="${icon}" alt="Weather icon for day ${idx + 1} in five-day forecast" />
+						<p class="temp">Temp: ${temp} °F</p>
+						<p class="wind">Wind: ${wind} MPH</p>
+						<p class="humidity">Humidity: ${humidity} %</p>
+					</div>`
+      )
+      .join("");
+
+    $(".forecast > .cards").html(forecastHTML);
+
+    if (!$(".forecast > h3").text()) {
+      $(".forecast > h3").text("5-Day Forecast");
+    }
+  }
+
+  updateForecasts(locationData, forecastData) {
+    const { date } = forecastData.today;
+    $(".summary > h2").text(
+      `${locationData.name} ${
+        date.getMonth() + 1
+      }/${date.getDate()}/${date.getFullYear()}`
+    );
+    this.updateHistory(locationData.name);
+    this.updateTodaysForecast(forecastData.today);
+    this.updateFiveDayForecast(forecastData.fiveDay);
   }
 
   updateTodaysForecast({ icon, temp, wind, humidity, uvi }) {
@@ -52,48 +93,11 @@ class WeatherUI {
       )}">${uvi.toString()}</div>`
     );
   }
-
-  updateFiveDayForecast(fiveDay) {
-    const newInnerHTML = fiveDay
-      .map(
-        ({ icon, date, temp, wind, humidity }, idx) =>
-          `<div class="card">
-                          <h3>${
-                            date.getMonth() + 1
-                          }/${date.getDate()}/${date.getFullYear()}</h3>
-                          <img src="${icon}" alt="Weather icon for day ${
-            idx + 1
-          } in five-day forecast" />
-                          <p class="temp">Temp: ${temp} °F</p>
-                          <p class="wind">Wind: ${wind} MPH</p>
-                          <p class="humidity">Humidity: ${humidity} %</p>
-                      </div>`
-      )
-      .join("");
-
-    $(".forecast > .cards").html(newInnerHTML);
-    $(".forecast > h3").text("5-Day Forecast");
-  }
-
-  updateForecasts(locationData, forecastData) {
-    // update title
-    const { date } = forecastData.today;
-    $(".summary > h2").text(
-      `${locationData.name} ${
-        date.getMonth() + 1
-      }/${date.getDate()}/${date.getFullYear()}`
-    );
-
-    // update rest of ui
-    this.updateHistory(locationData.name);
-    this.updateTodaysForecast(forecastData.today);
-    this.updateFiveDayForecast(forecastData.fiveDay);
-  }
 }
 
 class WeatherClient {
   constructor() {
-    this.ui = new WeatherUI();
+    this.ui = new WeatherUI(this);
     this.api = "https://api.openweathermap.org/data/2.5";
   }
 
@@ -119,7 +123,7 @@ class WeatherClient {
         humidity: current.humidity,
         uvi: current.uvi,
       },
-      fiveDay: daily.slice(1, 6).map((day) => ({
+      fiveDay: daily.slice(0, 5).map((day) => ({
         icon: `http://openweathermap.org/img/w/${day.weather[0].icon}.png`,
         date: new Date(day.dt * 1000),
         temp: day.temp.day,
@@ -133,27 +137,19 @@ class WeatherClient {
   async fetchLocationData(query) {
     const url = new URL(`${this.api}/weather`);
     url.search = new URLSearchParams({ q: query, appId: API_KEY });
-
     const res = await fetch(url);
     const json = await res.json();
 
     if (json?.cod === 200) {
-      return {
-        name: json.name,
-        lon: json.coord.lon,
-        lat: json.coord.lat,
-      };
+      return { name: json.name, lon: json.coord.lon, lat: json.coord.lat };
     }
 
-    return {
-      name: null,
-      lon: null,
-      lat: null,
-    };
+    return { name: null, lon: null, lat: null };
   }
 
   async search(query) {
     if (query) {
+      $("input.search-query").val("");
       const locationData = await this.fetchLocationData(query);
 
       if (locationData?.lon && locationData?.lat && locationData?.name) {
--- assets/main.js
+++ assets/main.js
@@ -2,11 +2,7 @@ var weatherClient = new WeatherClient();
 
 async function handleSearchClick() {
   const searchQuery = $('input[name="search-query"]').val();
-
-  if (searchQuery) {
-    return await weatherClient.search(searchQuery);
-  }
-
+  if (searchQuery) return await weatherClient.search(searchQuery);
   return alert("Please enter a search term.");
 }
 
